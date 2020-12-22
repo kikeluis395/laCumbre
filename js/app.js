a5f7f6cccd8a265e53f3d0163cbd53525bb83808
Luis Alvarado <kikeluis395@gmail.com>
2020-12-22 03:33:24 -0500

    Se agrego js en el header y se terminó el hero del proyecto

--- js/app.js
+++ js/app.js
@@ -8,9 +8,11 @@ window.addEventListener("scroll", function() {
     if (scrollTop > lastScrollTop) {
         top_navbar.style.display = "none";
         logo.style.display = "block";
+        menu_navbar.style.padding = "10px 0px"
     } else {
         top_navbar.style.display = "flex";
         logo.style.display = "none";
+        menu_navbar.style.padding = "2px 0px"
     }
     lastScrollTop = scrollTop;
 });
